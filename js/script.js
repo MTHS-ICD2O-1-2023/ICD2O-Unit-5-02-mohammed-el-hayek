62ad0e0ddca18a892f27dd8526272892aaa6bb79
mohammed-el-hayek <159185058+mohammed-el-hayek@users.noreply.github.com>
2024-04-26 02:46:50 +0000

    V 5.2.1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mohammed EL-hayek All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mohammed
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+const randomNumberPos = Math.floor(Math.random() * 6) + 1
+const randomNumberNeg = Math.floor(Math.random() * -6) + 1
+
+/**
+ * This function displays positive and negative number
+ */
+function myButtonClicked() {
+  buttonOnChecked = document.getElementById("positive.").checked
+
+  if (buttonOnChecked == true) {
+    document.getElementById("radio-button-value").innerHTML =
+      "Your number is: " + randomNumberPos + "."
+  } else {
+    document.getElementById("radio-button-value").innerHTML =
+      "Your number is: " + randomNumberNeg + "."
+  }
+}
